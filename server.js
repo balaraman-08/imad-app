4d103721ed4e8d680e2089d16cca6a695ae62252
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-18 23:12:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+    res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 
 app.get('/article-two', function(req, res){
-    res.send('Article two will be updated soon:)')
+    res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
 });
 
 app.get('/article-three', function(req, res){
-    res.send('Article three will be updated soon:)')
+    res.sendFile(path.join(__dirname, 'ui', 'article_three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
