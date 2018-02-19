d2818be63c3360f2bdb363d636772dd31f71d874
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-19 21:19:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function(){
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function(){
-        if(requestraedystate === XMLHttpRequest.DONE){
+        if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                     //Increment the view count
                     var count = request.responseText;
