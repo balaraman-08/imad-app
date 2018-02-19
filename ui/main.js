a24496e2f1c1c4ef17832b1136fcae4f13e66b15
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-19 20:50:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var button = document.getElementById('counter');
+var span = document.getElementById('count');
+var count = 0;
+
+button.onclick = function(){
+    count = count+1;
+    span.innerHtml = count.toString();
+}
