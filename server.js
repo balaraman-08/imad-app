842fc5f443c9c661dbae0d470b87ca8fdf9456b5
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-19 21:21:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+var count = 0;
+
+app.get('/counter', function (req, res) {
+    count = count+1;
+    res.send(count);
+});
+
 app.get('/ui/main.js', function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'main.js')); 
 });
