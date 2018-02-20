206048119fcc2563a244df9a1a7630649bc12cb4
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-20 23:58:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,20 +32,22 @@ submit.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                     //Increment the view count
-                    var count = request.responseText;
-                    var span = document.getElementById('count');
-                    span.innerHTML = count.toString();
+                        var name = name_input.value;
+                        var nameJSON = request.responseText;
+                        var names = JSON.parse(nameJSON);
+                        request.send(null);
+                        var list = "";
+                        for(var i = 0; i<names.length; i++){
+                            list += '<li>' + names[i] + '</li>';
+                        }
+                        
+                        ul.innerHTML = list;
+                    
             }
         }
     };
-    
-    var name = name_input.value;
-    var names = request.open('GET', "/submit/"+name, true);
+    //Requesting the name
+    request.open('GET', "/submit/"+name, true);
     request.send(null);
-    var list = "";
-    for(var i = 0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    
-    ul.innerHTML = list;
+
 };
