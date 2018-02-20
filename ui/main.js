f20d367e7402b8decfcbaaeb8f39643b64472f98
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-20 16:15:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,28 +4,6 @@ var button = document.getElementById('counter');
 
 var request = new XMLHttpRequest();
 
-button.onclick = function(){
-    
-    //Create the http request
-    
-    
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                    //Increment the view count
-                    var count = request.responseText;
-                    var span = document.getElementById('count');
-                    span.innerHTML = count.toString();
-                    console.log(count);
-            }
-        }
-    };
-    
-    //Make the request
-    request.open('GET', "/counter", true);
-    request.send(null);
-};
-
 window.onload = function(){
         request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
