e1782c85393b650c951927c36a166cdfe3b07c8a
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-20 23:31:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,9 +21,11 @@ window.onload = function(){
 
 var submit = document.getElementById('submit');
 var ul = document.getElementById('namelist');
+var name_input = documnet.getElementById('name');
 
 submit.onclick = function(){
-    var names = ['name1', 'name2', 'name3'];
+    var names = [];
+    names[names.length] = name_input.value;
     var list = "";
     for(var i = 0; i<names.length; i++){
         list += '<li>' + names[i] + '</li>';
