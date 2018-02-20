889c0d36f38fdd260e01c415eb5078972ec1cd70
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-21 00:15:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,3 +50,29 @@ submit.onclick = function(){
     request.open('GET', "http://balaramanmuthupandi.imad.hasura-app.io/submit?name="+name, true);
     request.send(null);
 };
+
+window.onclick = function(){
+        var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                    //Increment the view count
+                        var nameJSON = request.responseText;
+                        var names = JSON.parse(nameJSON);
+                        var list = "";
+                        for(var i = 0; i<names.length; i++){
+                            list += '<li>' + names[i] + '</li>';
+                        }
+                        
+                        ul.innerHTML = list;
+                    
+            }
+        }
+    };
+    //Requesting the name
+    var name_input = document.getElementById('name');
+    var name = name_input.value;
+    console.log(name);
+    request.open('GET', "http://balaramanmuthupandi.imad.hasura-app.io/submit?name="+name, true);
+    request.send(null);
+}
