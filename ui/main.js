011b27b3f3f868a1858772cbd79d9e4f7edbe654
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-20 16:14:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,17 @@ button.onclick = function(){
 };
 
 body.onload = function(){
+        request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                    //Increment the view count
+                    var count = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = count.toString();
+                    console.log(count);
+            }
+        }
+    };
     request.open('GET', "/counter", true);
     request.send(null);   
 };
