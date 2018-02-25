054b00093ab034206d33b973d202300134d57599
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-02-25 20:53:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/test-db', function(req, res){
     });
 });
 
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
     console.log(req.params.articleName);
     pool.query('SELECT * FROM article WHERE title = $1', [req.params.articleName], function(err, result){
         if (err){
