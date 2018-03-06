1cb8ab28f7979bab4f12fb3555a0239e46cfb7bc
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-03-06 18:41:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
-// var bodyParser = require('bodyParser');
+var bodyParser = require('bodyParser');
 
 var config = {
     user: 'balaramanmuthupandi',
@@ -16,7 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 function createTemplate(data) {
 
