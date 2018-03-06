852cca940df48edb1631e55bf44c597d8fc3a5d1
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-03-06 21:29:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,7 @@
 console.log('Loaded!');
 
-var button = document.getElementById('counter');
-
-
-
-window.onload = function(){
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                    //Increment the view count
-                    var count = request.responseText;
-                    var span = document.getElementById('count');
-                    span.innerHTML = count.toString();
-            }
-        }
-    };
-    request.open('GET', "/counter", true);
-    request.send(null);   
-};
 
 var submit = document.getElementById('submit');
-var ul = document.getElementById('namelist');
-var names = [];
 
 submit.onclick = function(){
     
@@ -30,23 +9,13 @@ submit.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                    //Increment the view count
-                        var nameJSON = request.responseText;
-                        var names = JSON.parse(nameJSON);
-                        var list = "";
-                        for(var i = 0; i<names.length; i++){
-                            list += '<li>' + names[i] + '</li>';
-                        }
-                        
-                        ul.innerHTML = list;
                     
             }
         }
     };
     //Requesting the name
-    var name_input = document.getElementById('name');
-    var name = name_input.value;
-    console.log(name);
-    request.open('GET', "http://balaramanmuthupandi.imad.hasura-app.io/submit?name="+name, true);
+    var username = document.getElementById('username');
+    
+    request.open('GET', "http://balaramanmuthupandi.imad.hasura-app.io/login", true);
     request.send(null);
 };
