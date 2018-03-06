998e5ed8126667fad1eb8933344c2ea497fbca18
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-03-06 18:37:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,21 +89,21 @@ app.get('/hash/:password', function(req, res){
 });
 
 //Creating user with password
-app.post('/create-user', function(req, res){
-   var name = req.body.username;
-   var password = req.body.password;
-   var salt = crypto.RandomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user2" (username, password) VALUES ($1, $2)', [name, dbString], function(err, result){
-       if(err){
-           res.status(500).send(err.toString);
-       }
-       else{
-           res.send("User created successfully: " + name);
-       }
-   });
+// app.post('/create-user', function(req, res){
+//   var name = req.body.username;
+//   var password = req.body.password;
+//   var salt = crypto.RandomBytes(128).toString('hex');
+//   var dbString = hash(password, salt);
+//   pool.query('INSERT INTO "user2" (username, password) VALUES ($1, $2)', [name, dbString], function(err, result){
+//       if(err){
+//           res.status(500).send(err.toString);
+//       }
+//       else{
+//           res.send("User created successfully: " + name);
+//       }
+//   });
    
-});
+// });
 
 app.get('/articles/:articleName', function(req, res){
     console.log(req.params.articleName);
