dbf82c415b5a05973ecf063aac7f189cbb66ed56
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-03-06 21:39:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,13 +9,24 @@ submit.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                    
+                alert('Login successfully');
+            }
+            else if(request.status === 403){
+                alert('Username/password is incorrect');
+            }
+            else if(request.status === 500){
+                alert('Something went wrong with server');
             }
         }
     };
+    
     //Requesting the name
-    var username = document.getElementById('username');
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     
     request.open('GET', "http://balaramanmuthupandi.imad.hasura-app.io/login", true);
-    request.send(null);
+    request.setRequestHeader('Content-Type', "application/json");
+    request.send(JSON.stringify({username: username, password: password}));
 };
