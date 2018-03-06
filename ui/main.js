abf178b56b33a640a159f6aab21e9ce922868b6e
balaraman-08 <33957876+balaraman-08@users.noreply.github.com>
2018-03-06 21:41:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ submit.onclick = function(){
     console.log(username);
     console.log(password);
     
-    request.open('GET', "http://balaramanmuthupandi.imad.hasura-app.io/login", true);
+    request.open('POST', "http://balaramanmuthupandi.imad.hasura-app.io/login", true);
     request.setRequestHeader('Content-Type', "application/json");
     request.send(JSON.stringify({username: username, password: password}));
 };
